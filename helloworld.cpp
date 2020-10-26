b379ce6e5634963811761e46059293823035b052
Edmond V <67634227+edvujic@users.noreply.github.com>
2020-10-26 11:44:39 +0100

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    
+    cout <<"Hello, World!";
+    // alternative usage printf("Hello, World!"); from C
+    /
+    return 0;
+}
